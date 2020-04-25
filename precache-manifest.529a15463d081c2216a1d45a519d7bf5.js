c8aca7b362f4b7a6b68f5771d1831eb3d859e90e
Sharath <sharath.prakash1992@gmail.com>
2020-04-26 02:54:15 +0800

    Updates

---
+++ precache-manifest.529a15463d081c2216a1d45a519d7bf5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0cdaeca352970e5b976e98eee9e612c",
+    "url": "/shop/index.html"
+  },
+  {
+    "revision": "b991b5a6845439606de3",
+    "url": "/shop/static/css/main.82f62d82.chunk.css"
+  },
+  {
+    "revision": "72cd48d03b5a8b8ac9a0",
+    "url": "/shop/static/js/2.74059b52.chunk.js"
+  },
+  {
+    "revision": "b013b7cd7a2da92c16a2d9932cfeb76c",
+    "url": "/shop/static/js/2.74059b52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b991b5a6845439606de3",
+    "url": "/shop/static/js/main.3e0ec1f9.chunk.js"
+  },
+  {
+    "revision": "52404f703f902e595678",
+    "url": "/shop/static/js/runtime-main.484f7a4d.js"
+  },
+  {
+    "revision": "6c426583a257819912278cedfdd9badf",
+    "url": "/shop/static/media/item1.6c426583.jpg"
+  },
+  {
+    "revision": "8779dd15782203466bfb67cac99ef2ec",
+    "url": "/shop/static/media/item2.8779dd15.jpg"
+  },
+  {
+    "revision": "9bd2546f523e4a6e2515068c65ca441c",
+    "url": "/shop/static/media/item3.9bd2546f.jpg"
+  },
+  {
+    "revision": "65bc33d70f7f4ca793c5acc24525de9f",
+    "url": "/shop/static/media/item4.65bc33d7.jpg"
+  },
+  {
+    "revision": "f5fd97b56137c3ed5df0f3f71482c105",
+    "url": "/shop/static/media/item6.f5fd97b5.jpg"
+  }
+]);
