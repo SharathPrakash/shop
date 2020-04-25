5d6ce030d57584b2b324a8b2fcc06b54a3701225
Sharath <sharath.prakash1992@gmail.com>
2020-04-26 01:56:23 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
             
               <Navbar/>
                 <Switch>
-                    <Route exact path="/" component={Home}/>
+                    <Route exact path="/home" component={Home}/>
                     <Route path="/cart" component={Cart}/>
                   </Switch>
              </div>
@@ -20,6 +20,7 @@ class App extends Component {
       
     );
   }
+  
 }
 
 export default App;
